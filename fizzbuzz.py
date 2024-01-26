35715875a73f1caf4ec32ff8aa7c6ac63fcb40f0
maddiegonzalez <maddiegonzalez@vt.edu>
2024-01-26 13:04:21 -0500

    Added FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for x in numbers:
+	if x%3==0 and x%5==0:
+		print("FizzBuzz")
+	if x%5!==0 and x%3==0:
+		print("Fizz)
+	if x%3!=0 and x%5==0:
+		print("Buzz")
+	if x%3!=0 and x%5!=0:
+		print(x)
